990c53daba200cdc4f92d31c7fd0c0844925f9f6
Jasper Op de Coul <jasper@infrae.com>
2008-07-22 13:41:51 +0200

    first commit, initial code setup

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(name='EURJel',
+      version=version,
+      description="EURJel code module",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      keywords='',
+      author='Jasper Op de Coul',
+      author_email='jasper@infrae.com',
+      url='',
+      license='BSD',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      include_package_data=True,
+
+      zip_safe=True,
+      install_requires=[
+    'nltk',
+      ],
+      )
